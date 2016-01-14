4888eb323017f393422b09d8f694f643b58e930a
Franky Liang <frankyliang@Frankys-Mac.local>
2016-01-13 23:45:49 -0500

    added loading screen

---
+++ Flicks-BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  Flicks-BridgingHeader.h
+//  MovieViewer
+//
+//  Created by Franky Liang on 1/13/16.
+//  Copyright © 2016 Franky Liang. All rights reserved.
+//
+
+#ifndef Flicks_BridgingHeader_h
+#define Flicks_BridgingHeader_h
+
+
+#endif /* Flicks_BridgingHeader_h */
