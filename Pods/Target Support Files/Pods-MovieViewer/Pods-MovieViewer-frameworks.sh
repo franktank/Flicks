902fcddb305be221f88558ed37a6044af2f7f183
Franky Liang <frankyliang@frankys-mac.local>
2016-01-16 21:04:53 -0500

    optionals

--- Pods/Target Support Files/Pods-MovieViewer/Pods-MovieViewer-frameworks.sh
+++ Pods/Target Support Files/Pods-MovieViewer/Pods-MovieViewer-frameworks.sh
@@ -85,7 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-MovieViewer/AFNetworking.framework"
+  install_framework "Pods-MovieViewer/EZLoadingActivity.framework"
+  install_framework "Pods-MovieViewer/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-MovieViewer/AFNetworking.framework"
+  install_framework "Pods-MovieViewer/EZLoadingActivity.framework"
+  install_framework "Pods-MovieViewer/MBProgressHUD.framework"
 fi
